f4afa341457419857369aba0ddb4748182cccd1e
khaleed2002 <zekomadred159@gmail.com>
2023-11-08 15:20:04 +0200

    update error about importing sourcing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Navbar from './components/Navbar'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import Header from './components/Header/header'
+import Header from './components/Header/Header'
 import ImagesContainer from './components/images/ImagesContainer'
 function App() {
-  
   return (
     <>
       <Navbar />
